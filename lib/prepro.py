f36803cbb118f24882290771b58f338bdc5f4f82
halc-00 <g2342055@tcu.ac.jp>
2025-10-01 20:11:49 +0900

    <feat(prepro):ISBN and publisher>

--- lib/prepro.py
+++ lib/prepro.py
@@ -12,6 +12,10 @@ def clean_df(df):
     df['本体価格'] = df['本体価格'].fillna(0)
     store_dummies = pd.get_dummies(df['書店コード'], prefix='store').astype(int)
     df = pd.concat([df, store_dummies], axis=1)
+    isbn_counts = df['ISBN'].value_counts().to_dict()
+    df['ISBN'] = df['ISBN'].map(isbn_counts).fillna(0).astype(int)
+    publisher_counts = df['出版社'].value_counts().to_dict()
+    df['出版社'] = df['出版社'].map(publisher_counts).fillna(0).astype(int)
 
     drop_columns=[
         '日付',
