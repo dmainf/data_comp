2c5265104a9e6c7a952073d081e02ffe62962b4b
dmainf <dmainf@MacBook-Pro.local>
2025-10-01 16:03:23 +0900

    add assesment code

--- lib/prepro.py
+++ lib/prepro.py
@@ -6,15 +6,13 @@ def clean_df(df):
     df['month'] = df['日付'].dt.month
     df['day'] = df['日付'].dt.day
     book_counts = df['書名'].value_counts().to_dict()
-    df['書名_encoded'] = df['書名'].map(book_counts).fillna(0).astype(int)
+    df['書名'] = df['書名'].map(book_counts).fillna(0).astype(int)
     author_counts = df['著者名'].value_counts().to_dict()
-    df['著者名_encoded'] = df['著者名'].map(author_counts).fillna(0).astype(int)
+    df['著者名'] = df['著者名'].map(author_counts).fillna(0).astype(int)
     df['本体価格'] = df['本体価格'].fillna(0)
 
     drop_columns=[
         '日付',
-        '書名',
-        '著者名'
     ]
     for column in drop_columns:
         df = df.drop(columns=[column])
